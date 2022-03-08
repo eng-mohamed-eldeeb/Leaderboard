c2bb6d0843325f54f43fce14c053ec54527353ea
mohamed eldeeb <73468629+eng-mohamed-eldeeb@users.noreply.github.com>
2022-03-08 20:09:13 +0200

    Fix lenter error #2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import './style.css';
 import addToStorage from './addData.js';
+
 const addForm = document.querySelector('.theForm');
 addForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  const name = form.querySelector('.name');
-  const score = form.querySelector('.score');
+  const name = addForm.querySelector('.name');
+  const score = addForm.querySelector('.score');
   addToStorage(name.value, score.value);
   name.value = '';
   score.value = '';
