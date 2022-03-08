ffa5fcace85c18a18a7c3030bd45fdf5f65d657c
mohamed eldeeb <73468629+eng-mohamed-eldeeb@users.noreply.github.com>
2022-03-08 20:00:11 +0200

    Add some functionality

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import './style.css';
-
+import addToStorage from './addData.js';
+// const data = JSON.parse(localStorage.getItem('data')) || [];
+const form = document.querySelector('.theForm');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const name = form.querySelector('.name');
+  const score = form.querySelector('.score');
+  addToStorage(name.value, score.value);
+  name.value = '';
+  score.value = '';
+});
