4022301ea1d3e3cd4a75d99234b8ffc88009d94c
mohamed eldeeb <73468629+eng-mohamed-eldeeb@users.noreply.github.com>
2022-03-09 21:04:42 +0200

    Use api to get and post data

---
+++ src/apiMethods.js
@@ -0,0 +1,24 @@
+const addScore = async (username, score) => {
+  await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/RbZstIDnGYdGTsk3sKDW/scores', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      user: username,
+      score,
+    }),
+  });
+};
+
+const getScores = async () => (
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/RbZstIDnGYdGTsk3sKDW/scores', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then((response) => response.json())
+    .then((data) => data.result)
+);
+
+export { addScore, getScores };
